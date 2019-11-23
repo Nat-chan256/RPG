8dc4023908347a1b02e810015af750c637ba1db5
Наталья Александровна Московка <s0136771@edu.kubsu.ru>
2019-11-23 17:39:29 +0300

    Добавьте файлы проекта.

---
+++ LayerClass.cpp
@@ -0,0 +1,33 @@
+﻿#include "LayerClass.h"
+#include <iostream>
+#include <vector>
+#include <SFML\Graphics.hpp>
+
+std::vector<int> Layer::operator [](int index)
+{
+	return tile_map[index];
+}
+
+void Layer::addTile(sf::Texture t, float x, float y, float w, float h)
+{
+	sf::Sprite s;
+	s.setTexture(t);
+	sf::IntRect rect(x, y, w, h);
+	s.setTextureRect(rect);
+
+	sprites.push_back(s);
+}
+
+void Layer::addTile(sf::Texture t, sf::Vector2i v1, sf::Vector2i v2)
+{
+
+	sf::Sprite s;
+	s.setTexture(t);
+	sf::IntRect rect(v1, v2);
+	s.setTextureRect(rect);
+
+	sprites.push_back(s);
+}
+
+void main()
+{}
